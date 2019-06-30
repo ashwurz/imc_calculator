1be91c669622fc90f6298b3839543773f38790aa
Ashwurz <helmuthfilho001@gmail.com>
2019-06-30 14:24:00 -0300

    Correção de bug no botão de refresh

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
     heightController.text = "";
     setState(() {
       _infoText = "Informe seus Dados!";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
